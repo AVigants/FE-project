f4e06c702dcb64ed0f627feb5f301b984c1e53f1
ArturW <artuursviigants@gmail.com>
2020-10-29 15:07:11 +0200

    started an addy class lets see how it goes

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,13 @@ $(window).on("load", function () {
     //---------------declaring variables to prevent crashes------------------
     let emailFind = {};
     let userArr = [];
-    let view = 'grid';
+    $('#table').hide();
+    let isAdmin = false;
+
     //--------------------------generate img src string function for template string when renedring-------------------------------------------
     let generateImg = () => {
         // return 'https://thispersondoesnotexist.com/image'
-        let base = 'https://picsum.photos/200/?random=';
+        let base = 'https://picsum.photos/400/?random=';
         let randomNum = Math.random().toString().slice(2, 4);
         return base + randomNum;
     }
@@ -43,6 +45,17 @@ $(window).on("load", function () {
     } else {
         renderData(userArr);
     }
+    //-------------------Admin features (adminMode)-----------------------------------------------
+    let adminMode = () => {
+        if(isAdmin){
+            $('.jumbotron').css({'background': 'gold'});
+            $('.btn-light').removeClass('btn-light').addClass('btn-primary');
+            $('.btn-secondary').removeClass('btn-light').addClass('btn-danger');
+        }
+        else{
+            alert('you are not authorized to use this feature');
+        }
+    }
     //-----------------------my welcome animation that Im probably going to abandon--------
 
 
@@ -51,118 +64,114 @@ $(window).on("load", function () {
     //----------------------adding functionality to buttons-------------------------------
     $('#becomeAdminBtn').click(e => {
         e.preventDefault();
-        console.log("you're about to become an admin. Proceed?")
+        adminMode();
     });
     $('#listViewBtn').click(e => {
-        view == 'list'
-        renderData(userArr);
+        $('#table').show();
+        $('#cards').hide();
     });
     $('#gridViewBtn').click(e => {
-        view = 'grid'
-        renderData(userArr);
+        $('#table').hide();
+        $('#cards').show();
     });
     $('#clearArrayBtn').click(e => {
         localStorage.removeItem('userArr');
     });
     $('#aboutBtn').click(e => {
         e.preventDefault();
-        console.log('about me page')
+        alert('yes, this is just a simple alert button describing this page...');
     });
     $('#logoutBtn').click(e => {
         localStorage.removeItem('emailFind');
     });
+
     //---------------------updating the dom with values from LS------------------------
     $('.jumbotron span').text(emailFind.fname);
 
     function renderData(data) {
-        if (view == 'grid') {
-            if (data.length > 16) {
-                for (let i = 0; i < 16; i++) {
-                    $('#cards').append(`
-                    <div class="col-md-6 col-lg-3 my-2 lmaoCards" id="${data[i].email}">
-                        <div class="card text-center">
-                            <img src="${generateImg()}" class="card-img-top img-fluid">
-                            <div class="card-block">
-                                <h3 class="card-title">${data[i].fname}</h3>
-                                <p>${data[i].email}</p>
-                            </div>
-                        </div>
-                    </div>`);
-                }
-            }
-        } else if (view == 'list') {
-            if (data.length > 16) {
-                $('#table').append(`
+        if (data.length > 16) {
+            //#table head
+            $('#table').append(`
                 <thead>
                     <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Actions</th>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Actions</th>
                     </tr>
                 </thead>
-                <tbody id="tbody">`);
-                for (let i = 0; i < 16; i++) {
-                    $('#tbody').append(`
+                <tbody id="tbody">
+            `);
+            for (let i = 0; i < 16; i++) {
+                //append data to #table
+                $('#tbody').append(`
                     <tr id="${data[i].email}">
-                    <th scope="row">${i + 1}</th>
-                    <td>${data[i].fname}</td>
-                    <td>${data[i].email}</td>
-                    <td>edit, del</td>
-                  </tr>
-                    `);
-                }
+                        <th scope="row">${i + 1}</th>
+                        <td>${data[i].fname}</td>
+                        <td>${data[i].email}</td>
+                        <td><button type="button" class="btn btn-light mr-1"><i class="fas fa-edit fa-lg"></i></button><button type="button" class="btn btn-secondary"><i class="fas fa-trash fa-lg"></i></button></td>
+                    </tr>
+                `);
 
+                //append data to #cards
+                $('#cards').append(`
+                <div class="col-md-6 col-lg-3 my-2 lmaoCards" id="${data[i].email}">
+                    <div class="card text-center">
+                        <img src="${generateImg()}" class="card-img-top img-fluid">
+                        <div class="card-block">
+                            <h3 class="card-title">${data[i].fname}</h3>
+                            <p>${data[i].email}</p>
+                                <button type="button" class="btn btn-light mr-1 float-left m-1"><i class="fas fa-edit fa-lg"></i></button><button type="button" class="btn btn-secondary float-right m-1"><i class="fas fa-trash fa-lg"></i></button>
+                        </div>
+                    </div>
+                </div>`);
             }
         }
+    //----------------------adding button mouseover functionality-----------------
+        $('button').hover(e=>{
+            if(!isAdmin){
+                // $('button').off('click');
+                $('button').css('cursor', 'not-allowed');
+            }
+        })   
     }
+    //-------------------------------User and Admin  classes-----------------------------------
+    class User {
+        constructor(fname, lname, email, pass, cpass) {
+            this.fname = fname,
+                this.lname = lname,
+                this.email = email,
+                this.pass = pass,
+                this.cpass = cpass
+        }
+    }
+    let user = new User ('fname', 'lname', 'email', 'pass', 'cpass');
 
-    //new idea = one function called render and inside it an if check (if view = grid then .... or if view = list then .... )
-
-
-
-    //-------------adding event listener to all cards, 2 buttons: edit and del-----------------------------
-    // $('.lmaoCards').hover(e=> {
-    //     $('.lmaoCards').css({'opacity': '0.5'});
-    //     console.log('.lmaoCards')
-    // });
-    //idk add 2 blurry buttons, man. Then make them functional after pressing become a member.
-
-    // <i class="fas fa-edit fa-lg"></i>
-    // <i class="fas fa-trash fa-lg"></i>
-
-    //also when I refresh the page and the remember me btn is pressed, it stays pressed. Maybe make it so it doesnt remember me when I click it asap?
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //--------------------laundry list----------------------------------------
-    //TODO when loggin out - remove emailFind from local storage
-    //add a clearAll to settings dropdown to remove all data from localstorage
-    // in index html add a class to the thanks you ad : tetxt-lowercase and text-capitalize for the name
-    //the template string only prints out the first name of my created users and the full name of the fake data.
-
-    //-------------comments----------------------------
-    //I probably didnt need to make a seperate object for 'remember me', I could have recycled 'emailFind' and called it a day
-    //add the animation to the app js file after loggin in, then the page gets redirected to main.html. that way when I refresh the main html page, it wont do the animation thingy every time...
-    //generate our own names, emails in japanese lol
-    //     ##randomArray ##arrayPush ##push ##forLoopScope
-    // const randomArray = [];
-    // for(let i = 0; i<67; i++){
-    //     randomArray.push(Math.random().toString().slice(2,5))
-    // };
-    // console.log(randomArray);
-    //yes I could make it so the next 16 items get rendered when I press a 'next page' btn but thats too much work for some random school assignment cmon now
+    class Admin extends User{
+        constructor(fname, lname, email, pass, cpass, isAdmin){
+            super(fname, lname, email, pass, cpass);
+            this.isAdmin = isAdmin;
+        }
+        deleteUser(user){
+            users = users.filter(u => u.username !== user.username);
+        }
+        editUser(user){
+            console.log(user);
+        }
+    }
+    let admin = new Admin('fname', 'lname', 'email', 'pass', 'cpass', false)
+    console.log(admin)
 
 });
+//fix the button thingy - when I dont hover over, become addy and then hover over = regular pointer. otherwise = alwasy not-allowed
+//fix the render condition -         if (data.length > 16) { ---- I dont like this statement. Could crash later
+
+
+//save the admin status
+//add buttons to grid
+//add btn functionality
+//add about page
+//animation?
+//save fakeData to local storage
+
+//paradoxically I could have used the last workflow from app.js and made an Admin class that extends the User class and store admin features in there but... I dont want to rework all this logic...
